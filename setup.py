d23acef28a02c0f1f4895928b50dd8fe1a007b89
corevfx <ma_yi@nyp.edu.sg>
2020-04-01 16:31:41 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 16:19:29 2020
+
+@author: Kevin Ma
+"""
+
+import setuptools
+
+setuptools.setup(
+    name="super_simple_graphics",
+    version="0.9.1",
+    author="Kevin Ma",
+    description="A super simple graphics lib for teaching designers python scripting",
+    url="https://github.com/corevfx/super_simple_graphics",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        ],
+    python_requires=">=3.7",
+    )
